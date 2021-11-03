91c536852ec3e93dbd245ee9028b1590ac5c1c2c
Marcio Kriiger <marciovonkriiger@gmail.com>
2021-11-03 14:45:55 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Date;
 import java.util.Locale;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +18,7 @@ public class Program {
 		//System.out.println(obj);
 		Seller seller = new Seller(21, "Bob", "bob@email.com",
 				new Date(), 3000.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 
 	}
